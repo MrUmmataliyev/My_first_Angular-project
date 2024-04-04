9222097ffdabdbd7c7d20d83a3749d62fbc14c4c
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-04-04 07:13:23 +0500

    Just created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', title: 'Home Page', component: HomeComponent},
+    {path:'', title: 'Home Page', component: HomeComponent},
+    {path:'about', title: 'About us', component: AboutComponent},
+    {path:'contact', title: 'Contacts', component: ContactComponent}
+];
